3c16bfa856616455af8a6405b2c7558a28b4e918
Alejandro Dietta <alexdietta@hotmail.com>
2018-07-07 22:32:55 +0200

    Momentjs installed, refactor using moment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+
+const moment = require('moment');
 const _ = require('underscore');
 
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September",
-  "October", "November", "December"];
-// Get current date info
+const days = moment.weekdays();
+const months = moment.months();
 const today = new Date().getDay();
 const currentYear = new Date().getFullYear();
-
 
 class App extends Component {
   
@@ -24,24 +23,17 @@ class App extends Component {
     return months.slice().map( month => {
       return  { 
         name: month,
-        days: _.range(1, this.getDaysInMonth(months.indexOf(month), year) + 1)
+        days: _.range(1, moment(`${year}-${moment().month(month).format('MM')}`, "YYYY-MM").daysInMonth()  + 1)
       };
     })
   }
  
   render() {
-    
     let calendarLikeArray = this.generateCalendar(currentYear);
-    console.log(calendarLikeArray)
 
     // If it is weekday
     if (today === 6 || today === 0)
-      return (
-        
-        <div>
-          <div>Today is {this.getDayName(today)} - You can watch TV all day today</div>
-        </div>
-      )
+      return <div>Today is {this.getDayName(today)} - You can watch TV all day today</div>;
 
     // If it is weeken
     return <div>Today is {this.getDayName(today)} - You should go to work today</div>;
