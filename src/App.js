e31dff23576d5b0c74d90f2adf8abb5f674010f8
Alejandro Dietta <alexdietta@hotmail.com>
2018-07-07 19:23:40 +0200

    Turorial #2 Date Checker

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div>Hello World</div>
-    );
+
+  getDayName(number) {
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return days[number];
   }
+
+  render() {
+    // Get current date
+    let today = new Date().getDay();
+    // If it is weekday
+    if (today === 6 || today === 0)
+      return <div>Today is {this.getDayName(today)} - You can watch TV all day today</div>;
+    // If it is weeken
+    return <div>Today is {this.getDayName(today)} - You should go to work today</div>;
+  }
+
 }
 
 export default App;
