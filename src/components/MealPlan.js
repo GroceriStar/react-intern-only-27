084429c4a9889b37358f001b35623ab2969eed50
Alejandro Dietta <alexdietta@hotmail.com>
2018-07-14 01:05:48 +0200

    Tutorial Part 6 #Test first approach

--- src/components/MealPlan.js
+++ src/components/MealPlan.js
@@ -3,20 +3,42 @@ import _ from 'lodash';
 
 class MealPlan extends Component {
   state = {
-    meal: '',
+    meal: {
+      name: '',
+      description: '',
+      image: ''
+    },
     meals: []
 };
 
   renderMeals() {
-    return _.map(this.state.meals, meal => <li>{meal}</li>);
+    return _.map(this.state.meals, meal => 
+      <div>
+        <img src={meal.image} alt="Smiley face" height="42" width="42"></img>
+        <li>{meal.name}</li>
+        <span>{meal.description}</span>
+      </div>
+    );
+  }
+
+  saveImage(event) {
+    this.setState({ meal: { ...this.state.meal, image: event.target.value }});
+  }
+  saveName(event) {
+    this.setState({ meal: { ...this.state.meal, name: event.target.value }});
+  }
+  saveDescription(event) {
+    this.setState({ meal: { ...this.state.meal, description: event.target.value }});
   }
 
   render() {
     return (
       <div>
         <h2>Today you should eat this</h2>
-        <input onChange={e => this.setState({meal: e.target.value})} value={this.state.meal} type="text" />
-        <button onClick={() => this.setState({meals: [...this.state.meals, this.state.meal]})}>
+        <input onChange={e => this.saveName(e)} value={this.state.meal.name} type="text" />
+        <textarea onChange={e => this.saveDescription(e)} value={this.state.meal.description} type="text" />
+        <input onChange={e => this.saveImage(e)} value={this.state.meal.image} type="text" />
+        <button onClick={() =>  this.setState({ meals: [...this.state.meals, this.state.meal] })}>
           Add meal
         </button>
         <ul>
