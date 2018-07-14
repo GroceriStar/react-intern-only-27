727b30aa4943fd36517919a479c0df0d0c0f2235
Alejandro Dietta <alexdietta@hotmail.com>
2018-07-14 20:35:04 +0200

    React Tutorial Part 7 DONE

--- src/components/MealPlan.js
+++ src/components/MealPlan.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 
-import InputMeal from  './InputMeal';
+import InputMeal from './InputMeal';
 
 class MealPlan extends Component {
   constructor(props) {
     super(props);
     this.state = {
       meal: {},
-      meals: []
+      meals: [],
+      showAddMealMessage: false
     };
-    
+
     this.saveMeal = this.saveMeal.bind(this);
     this.renderMeals = this.renderMeals.bind(this);
   }
@@ -18,7 +19,7 @@ class MealPlan extends Component {
   renderMeals() {
     return _.map(this.state.meals, meal =>
       <div>
-        <img src={meal.image} alt="Smiley face" height="42" width="42"></img>
+        <img src={meal.image || 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png'} alt="Meal Image" height="42" width="42"></img>
         <li>{meal.name}</li>
         <span>{meal.description}</span>
       </div>
@@ -28,19 +29,37 @@ class MealPlan extends Component {
   saveMeal(event) {
     event.preventDefault();
     const launch = {
-      name: event.target.name.value || '', 
+      name: event.target.name.value || '',
       image: event.target.image.value || '',
-      description: event.target.description.value || '' 
+      description: event.target.description.value || ''
     }
     this.setState({ meal: launch });
     this.setState({ meals: [...this.state.meals, launch] });
   }
 
+  componentWillMount() {
+    this.setState({
+      meals: [
+        { name: "Eggs with bacon" },
+        { name: "Spaghetti carbonara" },
+        { name: "Oatmeal" }
+      ]
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.meals.length < this.state.meals.length) {
+      this.setState({showAddMealMessage: true});
+      setTimeout(() => this.setState({showAddMealMessage: false}), 1000);
+    }
+  }
+
   render() {
     return (
-      <InputMeal 
-        onSendMeal={this.saveMeal} 
-        printMeals={this.renderMeals}>
+      <InputMeal
+        onSendMeal={this.saveMeal}
+        printMeals={this.renderMeals}
+        showAddMealMessage={this.state.showAddMealMessage}>
       </InputMeal>
     );
   }
